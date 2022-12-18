72697e5ddac9ea62d63973c8b80ce3ef5f367a63
Enerhim <himsharma9122006@gmail.com>
2022-12-18 16:25:58 +0530

    Basic Structure Successful

--- lightnet.py
+++ lightnet.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 '''
 Reference 
@@ -30,19 +31,55 @@ class NeuralNetwork:
         self.config = config
         self.act_functions = []
         
-    def init_params(self):
         # Initiate random weights and biases
-        self.weights = np.array()
-        self.biases = np.array()
+    def init_params(self):
+        self.weights = []
+        self.biases = []
+        for i in range(len(self.config)):
+            m = self.config[i]
+            
+            # Check if at last layer to generate K
+            try: k = self.config[i+1]
+            except IndexError: break
+            
+            # Add weights for entire layer to weights of network
+            # Weights of layer = Matrix(m, k)
+            # m is the input size and k is the output size
+            self.weights.append(np.random.randn(m, k)) 
+            self.biases.append(np.random.randn(1, k))
+            
+        self.weights = np.array(self.weights)
+        # self.biases = np.array(self.biases)
+                        
+    def activation(self, x):
+        return 1 / (1 + math.exp(-x))
+            
+
+    def feed_forward_once(self, input_):
+        # Input = Matrix(m, 1)
         
+        # Set activations to input layer initially
+        Z = input_
+        # Vectorize the activation function
+        act_vec = np.vectorize(self.activation)
+        # For each layer matrix 
+        for i, _ in enumerate(self.weights):
+            # Take the dot product of that layer matrix with input vector and add bias
+            Z = act_vec(np.dot(Z, self.weights[i]) + self.biases[i])
+        
+        return Z
+            
     
-    def feed_forward(self):
-        pass
-        
     def add_layer(self, no_neurons:int, activation:str):
         # Add length of layer to config
         self.config.append(no_neurons)
         # Add the activation function for that specific layer 
         self.act_functions.append(activation)    
-    
+        
 network = NeuralNetwork()
+network.add_layer(784, "sigmoid")
+network.add_layer(16, "sigmoid")
+network.add_layer(16, "sigmoid")
+network.add_layer(10, "sigmoid")
+network.init_params()
+print(network.feed_forward_once(np.random.randn(1, 784)) )
